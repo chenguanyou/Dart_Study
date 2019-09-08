bd319807acc5c7c9bd06c36a5b867aa4f646d9a6
chenguanyou <btmeiju@126.com>
2019-09-09 01:46:51 +0800

    if控制语句

---
+++ 03、控制语句/if语句.dart
@@ -0,0 +1,14 @@
+// if
+// if else if
+// if else if else
+
+void main() {
+  int frnction = 99;
+  if (frnction >= 90) {
+    print("学霸");
+  } else if (frnction >= 60) {
+    print("学弟");
+  } else {
+    print("学渣");
+  }
+}
