6896a5276703a9a203deb1a63a3a3bf41625464d
chenguanyou <btmeiju@126.com>
2019-09-09 01:17:33 +0800

    关系运算符

---
+++ 02、运算符/02、关系运算符.dart
@@ -0,0 +1,18 @@
+// 关系运算符主要用于两个值，两个变量的比较
+// 运算符：==，!=，>, <, >=, <=
+
+void main() {
+  int a = 100;
+  int b = 200;
+
+  print(a == b);
+  print(a != b);
+  print(a > b);
+  print(a < b);
+  print(a >= b);
+  print(a <= b);
+
+  String str1 = 'Hello';
+  String str2 = 'Hello';
+  print(str1 == str2);
+}
