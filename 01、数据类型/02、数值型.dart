4fc33956d78bede9810661cd7bb24f51dc03d882
chenguanyou <btmeiju@126.com>
2019-09-08 22:06:23 +0800

    数值型数据类型学习

--- 01、数据类型/02、数值型.dart
+++ 01、数据类型/02、数值型.dart
@@ -23,6 +23,20 @@ void main() {
   print(a ~/ b); // 取整运算
   print(a % b); // 取余运算
 
+  print(0.0 / 0.0); // 非数字类型
+
   // 常用属性：
   // isNan 是否是非数字，isEven 是否是偶数，isOdd 是否是基数。
+  print(a.isNaN); // 是否是非数字
+  print(b.isEven); // 是否是偶数
+  print(b.isOdd); // 是否是基数
+
+  // 常用方法
+  // abs(), round(), floor(), ceil(), toInt(), toDouble()
+  print(a.abs()); // 取绝对值
+  print(a.round()); // 四舍五入
+  print(a.floor()); // 不大于它取最大的整数
+  print(a.ceil()); // 不小于它的最大整数
+  print(a.toInt()); // 转换成int
+  print(a.toDouble()); // 转换成double
 }
